387b3cb0720399accefdfe8eb271d9c607eee8e6
unknown <Maria Nagy>
2018-04-11 14:09:06 +0300

    commit

--- src/de/mannheim/fourwins/GUIButton.java
+++ src/de/mannheim/fourwins/GUIButton.java
@@ -4,15 +4,15 @@ import java.awt.Button;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-class GUIButton
-extends Button
-implements ActionListener
-{
-// void klick() from extern
+@SuppressWarnings("serial")
+class GUIButton extends Button implements ActionListener {
+	// void klick() from extern
 
-GUIButton( String s ) {
-  super( s );
-  addActionListener( this );
-}
-public void actionPerformed( ActionEvent e ) {}
+	GUIButton(String s) {
+		super(s);
+		addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+	}
 }
